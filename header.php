fe8571433d8aec5d1c72e108b4eee4b8fb281076
Your Name <amk1005771@student.hamk.fi>
2025-02-20 19:52:46 +0200

    changes in header and linked message

--- header.php
+++ header.php
@@ -54,7 +54,7 @@
                 <a class="nav-link" href="home.php">Gallary</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="login.php">About</a>
+                <a class="nav-link" href="message.php">Contact Us</a>
               </li>
              
             </ul>
